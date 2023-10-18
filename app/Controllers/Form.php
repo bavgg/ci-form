37ced7d740d5894cad494182069ee4a56c1e6fc4
bavgg <gestopajonas@gmail.com>
2023-10-18 18:14:24 +0800

    rules added, getdata method added

--- app/Controllers/Form.php
+++ app/Controllers/Form.php
@@ -9,18 +9,33 @@ class Form extends BaseController
     public function index()
     {
         if (! $this->request->is('post')) {
+            echo "Hello";
             return view('signup');
         }
 
-        $rules = [];
+        $rules = [
+            'username' => 'required|max_length[30]',
+            'password' => 'required|max_length[255]|min_length[10]',
+            'passconf' => 'required|max_length[255]|matches[password]',
+            'email'    => 'required|max_length[254]|valid_email',
+        ];
 
         if (! $this->validate($rules)) {
+            echo "World";
             return view('signup');
         }
 
         // If you want to get the validated data.
         $validData = $this->validator->getValidated();
 
+        foreach($validData as $data){
+            echo $data;
+        }
+        
+
         return view('success');
+
+       
+        
     }
 }
